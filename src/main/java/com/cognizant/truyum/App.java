a7c388cf4d4714bf13afeeefd8e86772d9bb1b1b
Srideep Gurung <37174753+srideepg98@users.noreply.github.com>
2020-05-06 16:30:17 +0530

    springJpa

---
+++ src/main/java/com/cognizant/truyum/App.java
@@ -0,0 +1,16 @@
+package com.cognizant.truyum;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+@SpringBootApplication
+@ComponentScan(basePackages= {"com.cognizant.truyum, com.cognizant.truyum.dao, com.cognizant.truyum.controller,com.cognizant.truyum.model,com.cognizant.truyum.service, com.cognizant.truyum.repository,com.cognizant.truyum.util"})
+public class App {
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(App.class, args);
+
+	}
+}
+	
